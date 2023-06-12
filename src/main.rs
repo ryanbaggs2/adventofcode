32f08b42d4108225dafc0cb49be14f8bea331d33
Ryan Baggs <ryanbaggs7@gmail.com>
2023-06-12 15:15:49 -0500

    Add day_one.
    Add day_one with initial solution to problem.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::fs;
+use std::io::BufRead;
+
 fn main() {
-    println!("Hello, world!");
+    let file = fs::read("./input2").unwrap();
+
+    println!("{}", day_one(file))
 }
+
+fn day_one(file: Vec<u8>) -> i32 {
+    let mut total = 0;
+    let mut temp = 0;
+
+    for line in file.lines() {
+
+        let line_blah = line.unwrap();
+
+        if line_blah.is_empty() {
+            if temp > total {
+                total = temp;
+            }
+            temp = 0;
+        } else {
+            temp += line_blah.parse::<i32>().unwrap();
+        }
+    }
+
+    if temp > total {
+        return temp;
+    }
+
+    total
+}
+
+
